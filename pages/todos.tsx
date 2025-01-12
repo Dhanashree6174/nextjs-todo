30d22dcc6fd9a99f2da4adbcab5788e8e2b74c91
Dhanashree <dhanashreegosavi183@gmail.com>
2025-01-12 21:32:57 +0530

    Add feature to add new to-dos

--- pages/todos.tsx
+++ pages/todos.tsx
@@ -1,5 +1,54 @@
-const todos = () => {
-  return <div>To-Do App</div>;
+import { useState, useEffect } from "react";
+
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
 };
 
-export default todos;
+const Todos = () => {
+  const [todos, setTodos] = useState<Todo[]>([]); // need to specify type of array
+  const [newTodo, setNewTodo] = useState("");
+
+  // useEffect(() => {
+  //   fetch("/api/todos")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setTodos(data);
+  //       console.log(data);
+  //     });
+  // }, []);
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      { id: todos.length + 1, text: newTodo, completed: false },
+    ]);
+    setNewTodo("");
+  };
+
+  return (
+    <div>
+      <h1>My To-Dos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <span className="ml-5">
+              {todo.completed ? "Completed" : "Not Completed"}{" "}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Enter new to-do..."
+      />
+      <button onClick={addTodo}>Add To-Do</button>
+    </div>
+  );
+};
+
+export default Todos;
